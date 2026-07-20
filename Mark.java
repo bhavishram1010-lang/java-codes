5cfe97aea7e159bebc4f2d1962b94a36e079f2b1
bhavishram1010-lang <bhavish.ram1010@gmail.com>
2026-07-20 15:16:42 +0530

    Create Mark.java

---
+++ Mark.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class linearSearch{
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    
+    int[] marks={70,80,85,96,90};
+    String[] names={"bhavish","karthi","gokilan","guna","kiran"};
+    System.out.println("STUDENT MARK");
+    for(int i=0;i<marks.length;i++)
+       {
+         System.out.println(names[i]+" = "+marks[i]);
+}
+}
+}
